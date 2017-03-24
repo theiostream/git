fb49918b10e25c35645a2f8a83bae26ac7a175b0
Daniel Ferreira <daniel.calibeta@gmail.com>
2017-03-23 23:36:59 -0300

    remove_subtree(): reimplement using iterators
    Uses dir_iterator to traverse through remove_subtree()'s directory tree,
    avoiding the need for recursive calls to readdir() and simplifying code.
    
    Suggested in the GSoC microproject list, as well as:
    https://public-inbox.org/git/xmqqk27m4h3h.fsf@gitster.mtv.corp.google.com/
    
    A conversion similar in purpose was previously done at 46d092a
    ("for_each_reflog(): reimplement using iterators", 2016-05-21).
    
    Signed-off-by: Daniel Ferreira <bnmvco@gmail.com>

--- entry.c
+++ entry.c
@@ -2,6 +2,8 @@
 #include "blob.h"
 #include "dir.h"
 #include "streaming.h"
+#include "iterator.h"
+#include "dir-iterator.h"
 
 static void create_directories(const char *path, int path_len,
 			       const struct checkout *state)
@@ -46,29 +48,18 @@ static void create_directories(const char *path, int path_len,
 
 static void remove_subtree(struct strbuf *path)
 {
-	DIR *dir = opendir(path->buf);
+	struct dir_iterator *diter = dir_iterator_begin(path->buf);
 	struct dirent *de;
 	int origlen = path->len;
 
-	if (!dir)
-		die_errno("cannot opendir '%s'", path->buf);
-	while ((de = readdir(dir)) != NULL) {
-		struct stat st;
-
-		if (is_dot_or_dotdot(de->d_name))
-			continue;
-
+	while (dir_iterator_advance(diter) == ITER_OK) {
 		strbuf_addch(path, '/');
-		strbuf_addstr(path, de->d_name);
-		if (lstat(path->buf, &st))
-			die_errno("cannot lstat '%s'", path->buf);
-		if (S_ISDIR(st.st_mode))
-			remove_subtree(path);
-		else if (unlink(path->buf))
+		strbuf_addstr(path, diter->relative_path);
+		if (unlink(path->buf))
 			die_errno("cannot unlink '%s'", path->buf);
 		strbuf_setlen(path, origlen);
 	}
-	closedir(dir);
+
 	if (rmdir(path->buf))
 		die_errno("cannot rmdir '%s'", path->buf);
 }
